74f237f1c1168e3e6f3e0c0635a2edd0b861ebe9
arrowToTarget <1017635978@qq.com>
2016-05-25 11:28:55 +0800

    chagne

--- tspManger/src/main/java/com/lewis/tsp/Client.java
+++ tspManger/src/main/java/com/lewis/tsp/Client.java
@@ -1,11 +1,31 @@
 package com.lewis.tsp;
 
+import com.alibaba.fastjson.JSON;
+import com.lewis.tsp.strategy.query.QueryServiceTypeTspStrategy;
+import com.lewis.tsp.strategy.query.QuerySingleTspStrategy;
+
 /**
  * Created by zhangminghua on 2016/5/25.
  */
 public class Client {
 
     public static void main(String[] args) {
+        //单个tsp服务的查询
+        String tspName ="BOH.NM.ProductController.baokuanProduct";
+        System.out.println(JSON.toJSONString(QuerySingleTspStrategy.querySingleTsp(tspName)));
+        System.out.println("====================");
+        //按照tsp服务类型的查询
+        //查询价格服务的tsp信息
+        System.out.println(JSON.toJSONString(QueryServiceTypeTspStrategy.queryPriceServiceTypeTsp()));
+        //查询产品服务的tsp信息
+        System.out.println(JSON.toJSONString(QueryServiceTypeTspStrategy.queryProductServiceTypeTsp()));
+        //查询资源服务的tsp信息
+        System.out.println(JSON.toJSONString(QueryServiceTypeTspStrategy.queryResourceServiceTypeTsp()));
+        //查询透传服务的tsp信息
+        System.out.println(JSON.toJSONString(QueryServiceTypeTspStrategy.queryUnVarnishTransmissionServiceTypeTsp()));
+        //查询所有服务的tsp信息
+        System.out.println(JSON.toJSONString(QueryServiceTypeTspStrategy.queryAllServiceTypeTsp()));
+
 
     }
 
